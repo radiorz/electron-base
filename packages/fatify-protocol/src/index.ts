7bbafafed1ae051e86b63c84df5a7dd7277abfbd
tikkhun <s565338107@163.com>
2025-05-14 11:28:20 +0800

    chore: update.

--- packages/fatify-protocol/src/index.ts
+++ packages/fatify-protocol/src/index.ts
@@ -1,2 +1,7 @@
-import { hello } from '../lib';
-hello();
+import { createServer } from "../lib";
+import fastify from "fastify";
+console.log(`server is imported`);
+const server = fastify();
+server.get("/", () => "helloworld");
+server.get("/hello", () => "helloworld");
+createServer({ server }).bootstrap("app");
